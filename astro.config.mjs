a45a9cf1a1efdb44f5fd8dee6cd45289c93f6e85
jonathan <github.departure288@passmail.net>
2025-11-28 20:14:58 +0100

    update site URL and analytics script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: "https://jonathanwirz.com",
+  site: "https://jwirz.ch",
   integrations: [mdx(), sitemap(), tailwind()],
 });
